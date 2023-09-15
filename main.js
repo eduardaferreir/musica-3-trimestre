8c3696eddf9bab5df9d40bab6c5523ec3894269e
eduardaferreir <105672394+eduardaferreir@users.noreply.github.com>
2023-09-15 11:43:23 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,65 @@
+function tocaSomPom () {
+    document.querySelector('#som_tecla_pom').play();
+}
+
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
+
+
+function tocaSomClap () {
+    document.querySelector('#som_tecla_clap').play();
+}
+
+document.querySelector('.tecla_clap').onclick = tocaSomclap;
+
+
+function tocaSomTim () {
+    document.querySelector('#som_tecla_tim').play();
+}
+
+document.querySelector('.tecla_tim').onclick = tocaSomtim;
+
+
+function tocaSomPuff() {
+    document.querySelector('#som_tecla_puff').play();
+}
+
+document.querySelector('.tecla_puff').onclick = tocaSompuff;
+
+
+function tocaSomSplash () {
+    document.querySelector('#som_tecla_splash').play();
+}
+
+document.querySelector('.tecla_slaph').onclick = tocaSomslaph;
+
+
+function tocaToin () {
+    document.querySelector('#som_tecla_toin').play();
+}
+
+document.querySelector('.tecla_toin').onclick = tocaSomtoin;
+
+
+function tocaSomPsh() {
+    document.querySelector('#som_tecla_psh').play();
+}
+
+document.querySelector('.tecla_psh').onclick = tocaSompsh;
+
+
+function tocaSomTic () {
+    document.querySelector('#som_tecla_clap').tic();
+}
+
+document.querySelector('.tecla_tic').onclick = tocaSomtic;
+
+
+function tocaSomtom () {
+    document.querySelector('#som_tecla_tom').play();
+}
+
+document.querySelector('.tecla_tom').onclick = tocaSomtom;
+
+
+
+
